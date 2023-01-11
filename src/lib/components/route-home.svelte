d8de533b63c90e81b8ad5e3537ab4570b64809b0
Mike Cohen <mjoecohen@gmail.com>
2023-01-11 17:22:03 +0000

    Fix: link on logo to context path

--- src/lib/components/route-home.svelte
+++ src/lib/components/route-home.svelte
@@ -188,7 +188,7 @@ onDestroy(() => saveCurrentState());
 </script>
 <nav class="navbar navbar-expand-lg navbar-light bg-light" style="width: 100%;">
   <div class="container">
-    <a class="navbar-brand" href="/">
+    <a class="navbar-brand" href="/blocksim">
         <img width="50px" src={logo} alt="stacks ecosystem dao logo" />
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
